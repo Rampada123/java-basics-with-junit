a6f208d0fa3fca11dd2ee4af1fe760b61c8ec2ed
Rampada Manna <ram99manna@gmail.com>
2025-12-29 19:07:49 +0530

    Day 1: Added basic Calculator with JUnit tests

---
+++ src/main/java/com/rampada/basics/Calculator.java
@@ -0,0 +1,13 @@
+package com.rampada.basics;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calculator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
