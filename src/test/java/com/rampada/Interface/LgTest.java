cf1b72cec43f6329c233c6d05476431d6721b9f7
Rampada Manna <ram99manna@gmail.com>
2025-12-31 20:28:06 +0530

    Junit test

---
+++ src/test/java/com/rampada/Interface/LgTest.java
@@ -0,0 +1,48 @@
+package com.rampada.Interface;
+
+import static org.junit.Assert.assertEquals;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class LgTest {
+
+    private Remote remote;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUp() {
+        remote = new Lg();   // implementation under test
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    public void testOn() {
+        remote.on();
+        assertEquals(
+            "Switch on the remote" + System.lineSeparator(),
+            outContent.toString()
+        );
+    }
+
+    @Test
+    public void testOff() {
+        remote.off();
+        assertEquals(
+            "Switch off the remote" + System.lineSeparator(),
+            outContent.toString()
+        );
+    }
+
+
+}
+
