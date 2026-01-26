481f9c443e8166e71e1f44660fb370af0e72986d
Rampada Manna <ram99manna@gmail.com>
2026-01-26 12:00:15 +0530

    Constructor classes added

---
+++ src/main/java/com/rampada/constructor/Main.java
@@ -0,0 +1,13 @@
+package com.rampada.constructor;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Student s=new Student("Suman", 24);
+		
+		Student s1=new Student();
+		s1.details();
+		
+	}
+
+}
